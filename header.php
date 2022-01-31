90c2d12c6ca529e9c734f383a6c627faea29e492
karelpak1 <karelpak1@seznam.cz>
2022-01-31 10:44:16 +0100

    School - Rework 1

---
+++ header.php
@@ -0,0 +1,22 @@
+<div id="header">
+		<div>
+			<a href="index.html" id="logo"><img src="images/logo.png" alt="logo"></a>
+			<ul>
+				<li class="selected">
+					<a href="index.html">Home</a>
+				</li>
+				<li>
+					<a href="fotogalerie.html">Fotogalerie</a>
+				</li>
+				<li>
+					<a href="blog.html">Blog</a>
+				</li>
+				<li>
+					<a href="about.html">O nás</a>
+				</li>
+				<li>
+					<a href="kontakt.html">Kontakt</a>
+				</li>
+			</ul>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+        <?php
+            $titulky = ["uvod"=>"Zahrádky od profesionálů", "fotogalerie"=>"Záhrádky - Fotografie", "blog"=>"Zahrádky - Blog", "about"=>"Zahrádky - O nás", "kontakt"=>"Zahrádky - Kontakt"];
+
+            if (isset($_GET["strana"])) {
+                $str = $_GET["strana"];
+                echo "$titulky[$str]";
+            } else {
+                echo "Zahrádky od profesionálů";
+            }
+        ?>
+    </title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <main>
+        <header>
+            <?php include_once 'header.php';?>
+        </header>
+        <nav>
+        <!--     <?php include_once 'navigace.php';?>  -->
+        </nav>
+        <article>
+        <!--    <?php 
+                if (isset($_GET["strana"])) {
+                    $str = $_GET["strana"];
+                    include_once "$str.php";
+                }
+                else {
+                    include_once 'uvod.php';
+                }
+                ?> -->
+        </article>
+        <footer>
+              <?php 
+                include_once 'footer.php'; 
+              ?>
+        </footer>
+    </main>
+</body>
+</html>
