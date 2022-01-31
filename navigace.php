9c1a7423cd7716d5cd74501ee1a98470e1e136b4
karelpak1 <57904835+karelpak1@users.noreply.github.com>
2022-01-31 16:03:56 +0100

    fix

--- navigace.php
+++ navigace.php
@@ -1,15 +1,10 @@
 <div id="header">
 	<div>
-<<<<<<< HEAD
 		<a href="index.php" id="logo">
-=======
-		<a href="index.html" id="logo">
->>>>>>> 26714ac9c048200a3c6b5d4f56219edc07a235dc
             <img src="images/logo.png" alt="logo">
         </a>
 		<ul>
             <?php
-<<<<<<< HEAD
             // Název v URL adrese, provázaný na kliknutelný odkaz s textem
             $stranky = ["uvod"=>"Home", "fotogalerie"=>"Fotogalerie", "blog"=>"Blog", "about"=>"O nás", "kontakt"=>"Kontakt"];
 
@@ -21,16 +16,6 @@
                 echo "<li class='selected'";
                 echo ">";
                 echo "<a href='index.php?strana=$soubor'>$nazevmenu</a>";
-=======
-            $stranky = ["uvod"=>"Home", "fotogalerie"=>"Fotogalerie", "blog"=>"Blog", "about"=>"O nás", "kontakt"=>"Kontakt"];
-
-            foreach ($stranky as $soubor => $nazevpolozky) {
-                //soubor = uvod.php / fotogalerie.php / blog.php / about.php / kontakt.php
-                //nazevpolozky = Home / Fotogalerie / Blog / O nás / Kontakt
-
-                echo "<li>";
-                echo "<a href='index.php?strana=$soubor'>$nazevpolozky</a>";
->>>>>>> 26714ac9c048200a3c6b5d4f56219edc07a235dc
                 echo "</li>";
             }
             ?>
