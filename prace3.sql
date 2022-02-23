9db6c19dc87af38daf1bdfef27e3c032b10f08d0
karelpak1 <57904835+karelpak1@users.noreply.github.com>
2022-02-23 17:10:15 +0100

    Preparation of (all?) Files

---
+++ prace3.sql
@@ -0,0 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1:3306
+-- Vytvořeno: Stř 23. úno 2022, 16:00
+-- Verze serveru: 5.7.36
+-- Verze PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `prace3`
+--
+CREATE DATABASE IF NOT EXISTS `prace3` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `prace3`;
