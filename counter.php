e5a8622d37558f273db665bbcb5fddf3e7de91e1
karelpak1 <karelpak1@gmail.com>
2022-03-28 11:35:36 +0200

    School - Rework 2

--- counter.php
+++ counter.php
@@ -1,19 +1,29 @@
+<h1>Počitadlo přístupů</h1>
 <?php
 include 'pripoj.php';
 // Potřebuji změnit
 // Počítá se pomocí SQL databáze!
-$file = fopen("pocet.txt", "r+");
+$ip = $_SERVER["REMOTE_ADDR"];
+$dotaz = "INSERT INTO `counter` (`ip_adr`,`datum_a_cas`) VALUES ('$ip',NOW());";
 
-$pocet = fread($file, filesize("pocet.txt")) + 1;
-rewind($file);
-fwrite($file, $pocet);
+if (mysqli_query($condb, $dotaz)) {
+        echo "✅ Přístup na server zaznamenán 👌";
+    } 
+    else {
+        echo "❌ ERROR: ".mysqli_error($condb);
+    }
 
-fclose($file);
+// Prostě získáme maximum z autoinkrementace
+$dotaz = "SELECT MAX(poradi_navstiveni) FROM `counter`";
+$vysledekcount = mysqli_query($condb, $dotaz);
+$radek = mysqli_fetch_row($vysledekcount);
 
-echo "Jste ";
-include "pocet.txt";
-echo ". návštěvníkem tohoto souboru";
+echo '<h2>1. způsob:</h2>'.$radek[0];
 
-echo "<br>";
-echo "Jste $pocet. návštěvníkem tohoto souboru";
+$dotaz = "SELECT * FROM `counter`";
+$vysledek = mysqli_query($condb, $dotaz);
+$vysledekcount = mysqli_num_rows($vysledek);
+
+echo '<h2>2. způsob:</h2>'.$vysledekcount;
+
 ?>
